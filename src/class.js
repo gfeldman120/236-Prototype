aa710a5c2e3fbf70b305a081cf6b658e8eaf9e17
Aurora Wolf <ddw6891@g.rit.edu>
2026-04-16 15:48:07 -0400

    Made buttonClassGradesClicked() have relative links

--- src/class.js
+++ src/class.js
@@ -7,7 +7,7 @@ let buttonClassGrades;
 //When the user clicks buttonClassGrades
 //Goes to general grades page now, replace with class-specific grades page
 const buttonClassGradesClicked = () => {
-    window.open("/misc-pages/grades.html", "_self");
+    window.open("https://gfeldman120.github.io/236-Prototype/misc-pages/grades.html", "_self");
 }
 
 //Gets DOM elements and sets up events for them
